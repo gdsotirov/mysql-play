5f2c705e696b4ef989416eb66dd291f526af019c
gdsotirov <gdsotirov@gmail.com>
2018-04-12 16:52:56 +0300

    Example for bottom-up traversal

---
+++ Examples/WITH/cofc_for_emp.sql
@@ -0,0 +1,14 @@
+WITH RECURSIVE chain_cmd (empno, mgr, path) AS
+(
+SELECT empno, mgr, CAST(ename AS CHAR(200))
+  FROM emp
+ WHERE empno = 7788 /* SCOTT */
+UNION ALL
+SELECT EMP.empno, EMP.mgr, CONCAT(CCMD.path, ' <- ', EMP.ename)
+  FROM chain_cmd CCMD,
+       emp       EMP
+ WHERE EMP.empno = CCMD.mgr
+)
+SELECT *
+  FROM chain_cmd
+ WHERE mgr IS NULL;
