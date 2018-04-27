d2bd763a383beb79820f6059d538bec0c09b1d3b
gdsotirov <gdsotirov@gmail.com>
2018-04-27 13:07:15 +0300

    Escape path as it is a keyword

--- Examples/WITH/employee_paths.sql
+++ Examples/WITH/employee_paths.sql
@@ -1,15 +1,15 @@
-WITH RECURSIVE employee_paths (empno, ename, deptno, job, mgr, rang, path) AS
+WITH RECURSIVE employee_paths (empno, ename, deptno, job, mgr, rang, `path`) AS
 (
   SELECT empno, ename, deptno, job, empno, 1, CAST(ename AS CHAR(200))
     FROM emp
    WHERE mgr IS NULL
   UNION ALL
-  SELECT E.empno, E.ename, E.deptno, E.job, E.mgr, rang + 1, CONCAT(EP.path, ' <- ', E.ename)
+  SELECT E.empno, E.ename, E.deptno, E.job, E.mgr, rang + 1, CONCAT(EP.`path`, ' <- ', E.ename)
     FROM employee_paths EP,
          emp            E
    WHERE EP.empno = E.mgr
 )
-SELECT ECTE.empno, ECTE.ename, D.dname, ECTE.job, ECTE.rang, ECTE.path
+SELECT ECTE.empno, ECTE.ename, D.dname, ECTE.job, ECTE.rang, ECTE.`path`
   FROM employee_paths ECTE,
        dept           D
  WHERE ECTE.deptno = D.deptno
