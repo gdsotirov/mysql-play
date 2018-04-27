23a0038fe3ff2ad4a5411d7f9c9c46ac82048efa
gdsotirov <gdsotirov@gmail.com>
2018-04-27 13:04:01 +0300

    Split example

--- Examples/Window/simple.sql
+++ Examples/Window/simple.sql
@@ -6,20 +6,3 @@ SELECT E.ename, E.job, D.dname,
   FROM emp  E,
        dept D
  WHERE E.deptno = D.deptno;
-
-/* In MySQL 5.7 or older */
-
-SELECT E.ename, E.job, D.dname,
-       E.sal, dept_sal,
-       E.comm, dept_comm
-  FROM emp  E,
-       dept D,
-       (SELECT deptno, SUM(sal) dept_sal
-          FROM emp
-         GROUP BY deptno) DSAL,
-       (SELECT deptno, SUM(COALESCE(comm, 0)) dept_comm
-          FROM emp
-         GROUP BY deptno) DCOM
- WHERE E.deptno = D.deptno
-   AND DSAL.deptno = D.deptno
-   AND DCOM.deptno = D.deptno;
