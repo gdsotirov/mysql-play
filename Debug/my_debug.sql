78070108876a951fe0f9d9315962f18166ca1118
Georgi D. Sotirov <gdsotirov@gmail.com>
2018-08-22 12:18:21 +0300

    Example for tracing PSM stored routine

---
+++ Debug/my_debug.sql
@@ -0,0 +1,10 @@
+﻿DELIMITER //
+
+CREATE PROCEDURE my_debug(dbg_msg VARCHAR(1024))
+BEGIN
+  IF my_debug_enabled THEN
+    SELECT CONCAT('DEBUG: ', dbg_msg);
+  END IF;
+END //
+
+DELIMITER ;
---
+++ Debug/my_test_debug.sql
@@ -0,0 +1,13 @@
+﻿DELIMITER //
+
+CREATE PROCEDURE my_test_debug(IN a INTEGER, IN b INTEGER, OUT res INTEGER)
+BEGIN
+  CALL my_debug(CONCAT('enter my_test_debug(', a, ', ', b, ')'));
+
+  SET res := a + b;
+
+  CALL my_debug(CONCAT('exit my_test_debug(), res = ', res));
+END //
+
+DELIMITER ;
+
