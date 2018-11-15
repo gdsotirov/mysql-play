840648b1ce4b00fd418748c3a1f4ae8afba21f53
Georgi D. Sotirov <gdsotirov@gmail.com>
2018-11-15 20:29:55 +0200

    Add gen_range function that returns a random integer within a range

---
+++ DataMask/db/routines/gen_range.sql
@@ -0,0 +1,21 @@
+DELIMITER //
+
+CREATE FUNCTION gen_range(range_from INTEGER, range_to INTEGER)
+    RETURNS INT(11)
+    NO SQL
+BEGIN
+  DECLARE res INTEGER DEFAULT 0;
+
+  /* See https://dev.mysql.com/doc/refman/8.0/en/mathematical-functions.html#function_rand */
+  IF range_from = range_to THEN
+    SET res := range_from;
+  ELSEIF range_from < range_to THEN
+    SET res := FLOOR(range_from + RAND() * (range_to - range_from));
+  ELSE
+    SET res := FLOOR(range_to + RAND() * (range_from - range_to));
+  END IF;
+
+  RETURN res;
+END //
+
+DELIMITER ;
