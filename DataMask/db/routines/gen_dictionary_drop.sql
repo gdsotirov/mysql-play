2f96e5d078503857d9595c6f127d534053c87bb5
Georgi D. Sotirov <gdsotirov@gmail.com>
2018-11-18 17:26:15 +0200

    Add procedure gen_dictionary_drop for removal of dictionary (cannot be a function, beause uses prepared statement)

---
+++ DataMask/db/routines/gen_dictionary_drop.sql
@@ -0,0 +1,26 @@
+DELIMITER //
+
+CREATE PROCEDURE gen_dictionary_drop(dictionary_name VARCHAR(32))
+BEGIN
+  /* Note: Requires CREATE TEMPORARY TABLES privilege
+   * See https://dev.mysql.com/doc/refman/8.0/en/privileges-provided.html#priv_create-temporary-tables
+   */
+
+  IF dictionary_name NOT RLIKE '^[a-zA-Z]+$' THEN
+    SIGNAL SQLSTATE '99002'
+      SET MESSAGE_TEXT = 'Dictionary removal error (name not only alpha)';
+  END IF;
+
+  SET @drop_tmp_table_sttmnt_str := CONCAT('DROP TEMPORARY TABLE gen_dictionary_', dictionary_name);
+
+  PREPARE drop_tmp_table_sttmnt
+     FROM @drop_tmp_table_sttmnt_str;
+
+  EXECUTE drop_tmp_table_sttmnt;
+
+  DEALLOCATE PREPARE drop_tmp_table_sttmnt;
+
+  SELECT 'Dictionary removed';
+END //
+
+DELIMITER ;
