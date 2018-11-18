b854175deb1a01b66e0da661c1e2d52f5c196b1d
Georgi D. Sotirov <gdsotirov@gmail.com>
2018-11-18 12:18:34 +0200

    Check length and return NULL in case not 9 or 11 digits (formatted NNN-NN-NNNN) to match MySQL's implementation

--- DataMask/db/routines/mask_ssn.sql
+++ DataMask/db/routines/mask_ssn.sql
@@ -1,19 +1,19 @@
 DELIMITER //
 
-CREATE FUNCTION mask_ssn(ssn VARCHAR(12))
-    RETURNS VARCHAR(12) CHARSET utf8
+CREATE FUNCTION mask_ssn(ssn VARCHAR(11))
+    RETURNS VARCHAR(11) CHARSET utf8
     NO SQL
     DETERMINISTIC
 BEGIN
   DECLARE ssn_len INTEGER DEFAULT LENGTH(ssn);
-  DECLARE res_str VARCHAR(12) DEFAULT '';
+  DECLARE res_str VARCHAR(11) DEFAULT '';
 
-  IF ssn RLIKE '^[0-9]+$' THEN /* if passed as number */
+  IF ssn_len = 9 AND ssn RLIKE '^[0-9]+$' THEN /* if passed as number */
     SET res_str := mask_inner(ssn, 0, 4);
-  ELSEIF ssn RLIKE '^[0-9]{3}-[0-9]{2}-[0-9]{4}$' THEN /* if passed as ###-##-#### */
+  ELSEIF ssn_len = 11 AND ssn RLIKE '^[0-9]{3}-[0-9]{2}-[0-9]{4}$' THEN /* if passed as NNN-NN-NNNN */
     SET res_str := mask_outer(mask_inner(ssn, 4, 5), 3, 0);
   ELSE /* not recognized SSN */
-    SET res_str := '???-??-????';
+    SET res_str := NULL;
   END IF;
 
   RETURN res_str;
