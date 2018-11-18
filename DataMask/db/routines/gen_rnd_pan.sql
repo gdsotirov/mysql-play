d8aa062a6b37b2927637d76b917509f2cfbff570
Georgi D. Sotirov <gdsotirov@gmail.com>
2018-11-18 15:04:38 +0200

    Change return value to VARCHAR(19), use @gen_rnd_pan_len variable for lenght of the generated PAN and return NULL if less than 12, calculate and concatenate Luhn number to match MySQL's implementation

--- DataMask/db/routines/gen_rnd_pan.sql
+++ DataMask/db/routines/gen_rnd_pan.sql
@@ -1,18 +1,28 @@
 DELIMITER //
 
 CREATE FUNCTION gen_rnd_pan()
-    RETURNS CHAR(16) CHARSET utf8
+    RETURNS VARCHAR(19) CHARSET utf8
     NO SQL
 BEGIN
-  DECLARE res_pan CHAR(16) DEFAULT '';
+  DECLARE res_pan VARCHAR(19) DEFAULT NULL;
+  DECLARE idx INTEGER DEFAULT 0;
+  DECLARE sum_digits INTEGER DEFAULT 1;
 
-  /* TODO: This is just a string of 16 random numbers starting with a
-   * digit different than 0. The PAN possibly won't pass any verification
+  IF @gen_rnd_pan_len IS NULL THEN
+    SET @gen_rnd_pan_len := 16;
+  ELSEIF @gen_rnd_pan_len < 12 THEN
+    Set res_pan := NULL;
+  END IF;
+
+  /* 
    * See https://en.wikipedia.org/wiki/Payment_card_number
+   * See https://en.wikipedia.org/wiki/ISO/IEC_7812#Major_industry_identifier
    */
   SET @gen_rnd_string_characters := '0123456789';
-  SET res_pan := CONCAT(gen_range(1, 9), gen_rnd_string(15));
+  SET res_pan := gen_rnd_string(@gen_rnd_pan_len - 1);
   SET @gen_rnd_string_characters := NULL;
+
+  SET res_pan := CONCAT(res_pan, calc_luhn(res_pan));
 
   RETURN res_pan;
 END //
