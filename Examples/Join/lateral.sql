f9fc2f2d4b72546af19347fbf593731e76787eaa
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-01-23 14:17:05 +0200

    Complement example for solution with subqueries in SELECT, split solutions with comments, calculate also MIN and AVG

--- Examples/Join/lateral.sql
+++ Examples/Join/lateral.sql
@@ -1,23 +1,45 @@
 /* MySQL 8.0.14: Lateral derived tables */
 
-/* Calculate max salary per department */
-SELECT D.dname, DT.max_sal
+/* Problem: Calculate min/avg/max salary per department */
+
+/* Solution 1: with derrived table (and INNER JOIN) */
+SELECT D.dname, DT.min_sal, DT.avg_sal, DT.max_sal
   FROM dept D,
-       (SELECT E.deptno, MAX(E.sal) max_sal
+       (SELECT E.deptno, MIN(E.sal) min_sal, AVG(E.sal) avg_sal, MAX(E.sal) max_sal
           FROM emp E
          GROUP BY E.deptno
        ) AS DT
  WHERE DT.deptno = D.deptno;
 
-SELECT D.dname, DT.max_sal
+/* ... and with LEFT JOIN */
+SELECT D.dname, DT.min_sal, DT.avg_sal, DT.max_sal
   FROM dept D
        LEFT JOIN
-       (SELECT E.deptno, MAX(E.sal) max_sal
+       (SELECT E.deptno, MIN(E.sal) min_sal, AVG(E.sal) avg_sal, MAX(E.sal) max_sal
           FROM emp E
          GROUP BY E.deptno
        ) AS DT ON DT.deptno = D.deptno;
 
-/* Make derived table depend on the other table ? */
+/* Solution 2: with subqueries in SELECT */
+
+/* Subqueries in SELECT are restriced to 1 column */
+SELECT D.dname,
+       (SELECT E.deptno, MIN(E.sal) min_sal, AVG(E.sal) avg_sal, MAX(E.sal) max_sal
+          FROM emp E
+         WHERE E.deptno = D.deptno)
+  FROM dept D;
+
+/* Error Code: 1241. Operand should contain 1 column(s) */
+
+SELECT D.dname,
+       (SELECT MIN(E.sal) FROM emp E WHERE E.deptno = D.deptno) AS min_sal,
+       (SELECT AVG(E.sal) FROM emp E WHERE E.deptno = D.deptno) AS avg_sal,
+       (SELECT MAX(E.sal) FROM emp E WHERE E.deptno = D.deptno) AS max_sal
+  FROM dept D;
+
+/* Solution 3: Lateral derived table */
+
+/* Why not make the derived table depend on the other table? */
 SELECT D.dname, DT.max_sal
   FROM dept D,
        (SELECT MAX(E.sal) max_sal
@@ -28,10 +50,10 @@ SELECT D.dname, DT.max_sal
 /* Error Code: 1054. Unknown column 'D.deptno' in 'where clause' */
 
 /* LATERAL keyword */
-SELECT D.dname, LDT.max_sal
+SELECT D.dname, LDT.min_sal, LDT.avg_sal, LDT.max_sal
   FROM dept D,
        LATERAL
-       (SELECT MAX(E.sal) max_sal
+       (SELECT MIN(E.sal) min_sal, AVG(E.sal) avg_sal, MAX(E.sal) max_sal
           FROM emp E
          WHERE E.deptno = D.deptno
        ) AS LDT;
