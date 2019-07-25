784874270849601f16c45770f6082f3dc000bc03
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-07-25 15:55:23 +0300

    Add script for stripping data from MySQL dump files

---
+++ Scripts/strip_mydump_data.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Strip data from MySQL dump flles
+# Written by Georgi D. Sotirov <gdsotirov@gmail.com>
+# See https://stackoverflow.com/a/37681075 for sed multiline delete example
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <file>"
+fi
+
+# The following expressions matches and deletes:
+# - Dumping data for table comment banners
+# - INSERT INTO statments
+cat "$1" | sed -e '/^\-\-$/ {N;N; }; /Dumping data for table/d' \
+               -e '/^INSERT INTO/ { :e;N;/;$/!be }; /^INSERT INTO/d' > "${1}.nodata"
+
