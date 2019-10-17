e75d7ae5136bdb472eb6fb2dec5a0af4aa155fe3
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-10-17 20:44:58 +0300

    Add hash join example

---
+++ Examples/Join/hash_join.sql
@@ -0,0 +1,38 @@
+/* Hash join optimization was added in MySQL 8.0.18
+ * See https://dev.mysql.com/doc/refman/8.0/en/hash-joins.html
+ */
+
+/* Create table for salary ranges by job */
+CREATE TABLE job_sal (
+  job     VARCHAR(9),
+  sal_min DECIMAL(9,2),
+  sal_max DECIMAL(9,2)
+);
+
+/* Load some data */
+INSERT INTO job_sal (job, sal_min, sal_max) VALUES ('ANALYST', 3000, 4000);
+INSERT INTO job_sal (job, sal_min, sal_max) VALUES ('CLERK', 800, 1500);
+INSERT INTO job_sal (job, sal_min, sal_max) VALUES ('MANAGER', 2800, 3500);
+INSERT INTO job_sal (job, sal_min, sal_max) VALUES ('SALESMAN', 1250, 1900);
+INSERT INTO job_sal (job, sal_min, sal_max) VALUES ('PRESIDENT', 5000, NULL);
+
+/* Select employees with salary out of range */
+EXPLAIN FORMAT=TREE
+SELECT E.ename, E.sal, JS.sal_min, JS.sal_max
+  FROM emp     E,
+       job_sal JS
+ WHERE E.job = JS.job
+   AND E.sal NOT BETWEEN JS.sal_min AND JS.sal_max;
+
+/* 0.89 sec for 1M employees */
+
+/* ... and without hash join optimization */
+EXPLAIN
+SELECT /*+ NO_HASH_JOIN(JS, E) */E.ename, E.sal, JS.sal_min, JS.sal_max
+  FROM emp     E,
+       job_sal JS
+ WHERE E.job = JS.job
+   AND E.sal NOT BETWEEN JS.sal_min AND JS.sal_max;
+
+/* 1.26 sec for 1M employees */
+
