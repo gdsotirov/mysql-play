08e06bd5027e48c12a8b69a94d0e6dae13c1f40b
Georgi D. Sotirov <gdsotirov@gmail.com>
2019-10-29 14:32:36 +0200

    Update bug 95192 test case (fixed in 8.0.18)

--- Examples/CHECK/bug_95192_test_case.sql
+++ Examples/CHECK/bug_95192_test_case.sql
@@ -14,9 +14,13 @@ CREATE TABLE tst (
 /* Query OK, 0 rows affected */
 
 /* 2. Insert a valid record */
-INSERT INTO tst (id, start_date) VALUES (1, '2019-04-29');
+INSERT INTO tst (id, start_date) VALUES (1, '2019-10-29');
 /* Query OK, 1 row affected */
 
 /* 3. Try to insert an invalid record... suprise! */
-INSERT INTO tst (id, start_date) VALUES (2, '2019-04-25'); /* Should fail, but suceeds */
+INSERT INTO tst (id, start_date) VALUES (5, '2019-10-25');
+/* Should fail, but suceeds in MySQL 8.0.16 and MySQL 8.0.17 */
 /* Query OK, 1 row affected */
+/* Properly fails in MySQL 8.0.18 */
+/* Error Code: 3819. Check constraint 'chk_dat' is violated. */
+
