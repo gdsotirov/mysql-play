b2b0a9e9cd6106fae0c5822916831379a664bc39
Georgi D. Sotirov <gdsotirov@gmail.com>
2020-05-09 10:51:35 +0300

    Rename columns to a, b, c

--- Examples/WITH/pythagorean_triples.sql
+++ Examples/WITH/pythagorean_triples.sql
@@ -3,6 +3,7 @@
  * TODO: Reduce to only primitives, e.g. exclude (6, 8, 10) as it is
  *       a multiple of (3, 4, 5).
  */
+
 WITH RECURSIVE nums AS
 (SELECT 1 AS n
  UNION ALL
@@ -10,12 +11,12 @@ WITH RECURSIVE nums AS
    FROM nums
   WHERE n < 100
 )
-SELECT x.n AS x, y.n AS y, z.n AS z
+SELECT x.n AS a, y.n AS b, z.n AS c
   FROM nums x,
        nums y,
        nums z
  WHERE pow(x.n, 2) + pow(y.n, 2) = pow(z.n, 2)
    AND x.n < y.n
    AND y.n < z.n
- ORDER BY x.n, y.n, z.n;
+ ORDER BY a, b, c;
 
