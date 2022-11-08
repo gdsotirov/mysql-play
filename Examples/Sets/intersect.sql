6f98df774f7c203e809253d08ce9415114867951
Georgi D. Sotirov <gdsotirov@gmail.com>
2022-11-08 18:19:54 +0200

    Update INTERSECT example for MySQL 8.0.31

--- Examples/Sets/intersect.sql
+++ Examples/Sets/intersect.sql
@@ -1,12 +1,68 @@
-﻿/* Still not possible
+﻿/* MySQL 8.0.31 finally added INTERSECT clause
+ * See https://dev.mysql.com/doc/refman/8.0/en/intersect.html and
+ *     https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-31.html#mysqld-8-0-31-sql-syntax
+ */
 
-SELECT id, name FROM a
+/* The following would return only the positive numbers from a, because they
+ * are also present in b
+ */
+
+WITH RECURSIVE
+/* all numbers from 1 to 10 */
+a AS (
+  SELECT 1 AS n
+  UNION ALL
+  SELECT n + 1
+    FROM a
+   WHERE n < 10
+),
+/* only positive numbers from 1 to 10 */
+b AS (
+  SELECT 2 AS n
+  UNION ALL
+  SELECT n + 2
+    FROM b
+   WHERE n < 10
+)
+SELECT *
+  FROM a
 INTERSECT
-SELECT id, name FROM b;
+SELECT *
+  FROM b;
 
-but possible with INNER JOIN
+/* Result:
+ * +------+
+ * | n    |
+ * +------+
+ * |    2 |
+ * |    4 |
+ * |    6 |
+ * |    8 |
+ * |   10 |
+ * +------+
+ * 5 rows in set (0,00 sec)
+ */
 
-*/
+/* The same is also possible with INNER JOIN, which was the only
+ * option with previous versions
+ */
 
-SELECT a.id, a.name
-  FROM a INNER JOIN b USING (id, name);
+WITH RECURSIVE
+/* all numbers from 1 to 10 */
+a AS (
+  SELECT 1 AS n
+  UNION ALL
+  SELECT n + 1
+    FROM a
+   WHERE n < 10
+),
+/* only positive number from 1 to 10 */
+b AS (
+  SELECT 2 AS n
+  UNION ALL
+  SELECT n + 2
+    FROM b
+   WHERE n < 10
+)
+SELECT *
+  FROM a INNER JOIN b USING (n);
