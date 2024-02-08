d5a29e437e769e7bc4721007e8429604b7d85191
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-02-08 19:52:16 +0200

    Add comments for SQL standard and database support

--- Fun/tree.sql
+++ Fun/tree.sql
@@ -1,5 +1,10 @@
-/* A Christmas tree with SQL using Common Table Expressions (CTE)
+/* A Christmas tree with SQL using Common Table Expressions (CTE) that were
+ * introduced with SQL:1999 standard (see https://en.wikipedia.org/wiki/SQL:1999)
  * Inspired by SQL Daily's tweet - see https://twitter.com/sqldaily/status/1077534562518032390
+ * Should work in:
+ * - MariaDB >= 10.2.1 (see https://mariadb.com/kb/en/mariadb-1021-release-notes/);
+ * - MySQL >= 8.0.1 (see https://dev.mysql.com/doc/relnotes/mysql/8.0/en/news-8-0-1.html); and
+ * - PostgreSQL >= 8.4.0 (see https://www.postgresql.org/docs/release/8.4.0/)
  */
 
 WITH RECURSIVE tree (i, t) AS (
