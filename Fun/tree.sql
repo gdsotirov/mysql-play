01adf7526a414838c6a0d685b512c579e8f2fe10
Georgi D. Sotirov <gdsotirov@gmail.com>
2024-02-08 19:28:55 +0200

    Remove CAST, add join condition
    Remove CAST and add dummy join condition, so query runs in Postgres.

--- Fun/tree.sql
+++ Fun/tree.sql
@@ -5,12 +5,12 @@
 WITH RECURSIVE tree (i, t) AS (
   WITH args AS (SELECT 5 AS height)
   SELECT 1 AS i,
-         CAST(CONCAT(LPAD(' ', args.height, ' '), LPAD('*', 1, '*')) AS CHAR(128)) AS t
+         CONCAT(LPAD(' ', args.height, ' '), LPAD('*', 1, '*')) AS t
     FROM args
   UNION ALL
   SELECT i + 1 AS i,
          CONCAT(LPAD(' ', args.height - i, ' '), LPAD('*', (i * 2) + 1, '*')) AS t
-    FROM tree JOIN args
+    FROM tree JOIN args ON 1 = 1 /* for Postgres */
    WHERE tree.i < args.height
 )
 SELECT t AS TREE
